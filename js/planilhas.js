8c187bdab4d7f3ea917033c03f7e684ac0df0231
mrbrunelli <matheus.brunelli@gmail.com>
2019-10-07 16:07:48 -0300

    nova página criada em javascript

---
+++ js/planilhas.js
@@ -0,0 +1,77 @@
+const planilhas = [
+    {
+        id: '1',
+        title: 'Gazin Atacado',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '29-12-2019'
+    },
+
+    {
+        id: '2',
+        title: 'Gazin GeoMarketing',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '12-07-2018'
+    },
+
+    {
+        id: '3',
+        title: 'Gazin Consórcio',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '14-06-2019'
+    },
+
+    {
+        id: '4',
+        title: 'Gazin Varejo',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '01-01-2019'
+    },
+
+    {
+        id: '5',
+        title: 'Gazin Colchões',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '24-02-2017'
+    },
+
+    {
+        id: '6',
+        title: 'Gazin Geral',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '04-08-2005'
+    },
+    {
+        id: '7',
+        title: 'Gazin Administração',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '22-11-2016'
+    },
+    {
+        id: '8',
+        title: 'Gazin Industria',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '21-01-2014'
+    },
+    {
+        id: '9',
+        title: 'Gazin Celulares',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '02-05-2017'
+    },
+    {
+        id: '10',
+        title: 'Gazin Geral do geral',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum saepe hic minus quasi ipsam possimus quibusdam voluptates nostrum error, magni rerum omnis ea beatae esse tenetur repellat eaque dignissimos maxime.',
+        img: 'googleSheets.png',
+        data: '19-04-2002'
+    },
+]
